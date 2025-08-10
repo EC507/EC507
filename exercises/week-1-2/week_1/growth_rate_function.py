69e2fa484021e57d51bcc04a0e93d86ae2de2cfc
sudha18905 <sudhasharma18905@gmail.com>
2025-08-10 14:54:11 +0530

    d

--- exercises/week-1-2/week_1/growth_rate_function.py
+++ exercises/week-1-2/week_1/growth_rate_function.py
@@ -1,6 +1,10 @@
-def growth_rate(current, previous):
-    if previous == 0:
-        return None
-    return (current - previous) / previous * 100
 
-print(growth_rate(120, 100))  # Example usage
+
+
+
+
+
+a=(input("first no:"))
+b=(input("second no:"))
+
+print(a>b)
